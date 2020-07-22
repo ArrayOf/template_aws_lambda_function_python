2268f6c9edc3ef194a38f6d24473194cec59db9c
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2020-07-22 15:38:56 -0300

    Update publish.sh

--- {{cookiecutter.function_name}}/publish.sh
+++ {{cookiecutter.function_name}}/publish.sh
@@ -5,6 +5,6 @@ source activate
 function_name="{{cookiecutter.function_name}}"
 zip_file="function.zip"
 
-aws lambda update-function-code --function-name "${function_name}" --zip-file "fileb://${zip_file}" --profile sofie --region sa-east-1
+aws lambda update-function-code --function-name "${function_name}" --zip-file "fileb://${zip_file}" --profile profile --region region
 
 rm -rf "${zip_file}"
